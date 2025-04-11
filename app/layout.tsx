a87b5f06af6392c36a3153dfa872d67aaee925c7
Prasaja Mukti <31468979+orepras@users.noreply.github.com>
2025-04-11 23:00:02 +0700

    adding vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next"
 import { Inter, Noto_Serif } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import Link from "next/link"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ 
   subsets: ["latin"],
